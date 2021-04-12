530af683c1681cf9977bd859a26c318d63cc6ece
Jessica Caldas <jessicacaldas18@gmail.com>
2021-04-12 12:14:55 +0100

    Adapter

--- app/src/main/java/estg/ipvc/app/MainActivity.kt
+++ app/src/main/java/estg/ipvc/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package estg.ipvc.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import estg.ipvc.app.adapter.NotaAdapter
+import estg.ipvc.app.viewModel.NotaViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //recycler view
+        val recyclerView = findViewById<RecyclerView>(R.id.recycleview)
+        val adapter = NotaAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //view model
+        NotaViewModel = ViewModelProvider(this).get(NotaViewModel::class.java)
+        NotaViewModel.allNotas.observe(this, { notas ->
+            notas?.let{adapter.setNotas(it)}
+        })
+
+        //Fab
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddNota::class.java)
+            startActivityForResult(intent, newNotaActivityRequestCode)
+        }
     }
 }
---
+++ app/src/main/java/estg/ipvc/app/adapter/NotaAdapter.kt
@@ -0,0 +1,39 @@
+package estg.ipvc.app.adapter
+
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import estg.ipvc.app.R
+import estg.ipvc.app.entities.Nota
+
+class NotaAdapter internal constructor(context: Context): RecyclerView.Adapter<NotaAdapter.NotaViewHolder>(){
+
+    private val inflater:LayoutInflater = LayoutInflater.from(context)
+    private var notas = emptyList<Nota>()
+
+    class NotaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val notaItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder{
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent,  false)
+        return NotaViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NotaViewHolder, position: Int) {
+        val current = notas[position]
+        holder.notaItemView.text = current.nota
+    }
+
+    internal fun setNotas(notas: List<Nota>){
+        this.notas = notas
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = notas.size
+
+}
